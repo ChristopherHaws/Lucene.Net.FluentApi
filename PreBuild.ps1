0baac498e796388bc30566e1e92035aa8539886e
Christopher Haws <cribs2@gmail.com>
2015-08-01 03:26:18 -0700

    Added nuget restore to prebuild

--- PreBuild.ps1
+++ PreBuild.ps1
@@ -22,3 +22,5 @@ else
 
  # run DNU restore on all project.json files in the src folder including 2>1 to redirect stderr to stdout for badly behaved tools
 Get-ChildItem -Path $PSScriptRoot\src -Filter project.json -Recurse | ForEach-Object { & dnu restore $_.FullName 2>1 }
+
+NuGet restore
