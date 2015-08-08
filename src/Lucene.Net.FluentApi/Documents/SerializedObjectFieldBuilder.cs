579f53e8571bf4116ecb01bbfc4ee77ca969c35f
Christopher Haws <cribs2@gmail.com>
2015-08-07 23:20:22 -0700

    Added Compresses to SerializedObjectFieldBuilder

--- src/Lucene.Net.FluentApi/Documents/SerializedObjectFieldBuilder.cs
+++ src/Lucene.Net.FluentApi/Documents/SerializedObjectFieldBuilder.cs
@@ -3,10 +3,11 @@ using Lucene.Net.Extentions;
 
 namespace Lucene.Net.Documents
 {
-	public class SerializedObjectFieldBuilder
+	public class SerializedObjectFieldBuilder : ISerializedObjectFieldBuilder
 	{
 		private readonly Document document;
 		private readonly Object value;
+		private readonly FieldStoreBuilder<ISerializedObjectFieldBuilder> storedBuilder;
 
 		public SerializedObjectFieldBuilder(Document document, Object value)
 		{
@@ -22,6 +23,12 @@ namespace Lucene.Net.Documents
 
 			this.document = document;
 			this.value = value;
+			this.storedBuilder = new FieldStoreBuilder<ISerializedObjectFieldBuilder>(this);
+		}
+
+		public ISerializedObjectFieldBuilder Compressed()
+		{
+			return this.storedBuilder.Store();
 		}
 
 		public void As(String name)
@@ -31,8 +38,15 @@ namespace Lucene.Net.Documents
 				throw new ArgumentNullException(nameof(name));
 			}
 
+			var stored = this.storedBuilder.ToFieldStore();
+
 			var valueBytes = this.value.BinarySerialize();
-			this.document.Add(new Field(name, valueBytes, Field.Store.YES));
+			this.document.Add(new Field(name, valueBytes, stored));
 		}
+	}
+
+	public interface ISerializedObjectFieldBuilder : IFieldBuilder
+	{
+		ISerializedObjectFieldBuilder Compressed();
 	}
 }
